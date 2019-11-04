b8501e6254f42347ea7c28e57530bc8652423d25
ShobhitRathi <shobhitrathi10@gmail.com>
2019-11-04 16:12:04 +0530

    currently in work
    have some issues

---
+++ prime-number.py
@@ -0,0 +1,19 @@
+x=int(input("enter"))
+
+for i in range(2,x):
+        z=x%i
+        if z!=0:
+                f=0
+                ##print(f)
+                if f==0:
+                        print("prime")
+                        break
+        else:
+                f=1
+                break
+                if f==1:
+                        print("composite")
+                        break
+                ##print(f)
+
+
