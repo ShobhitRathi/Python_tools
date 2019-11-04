4e6f72d10419fb634a836b3b20480e6a9a269b6b
ShobhitRathi <shobhitrathi10@gmail.com>
2019-11-04 22:22:20 +0530

    Update prime-number.py
    fixed issue #1

--- prime-number.py
+++ prime-number.py
@@ -4,16 +4,11 @@ for i in range(2,x):
         z=x%i
         if z!=0:
                 f=0
-                ##print(f)
-                if f==0:
-                        print("prime")
-                        break
         else:
                 f=1
                 break
-                if f==1:
-                        print("composite")
-                        break
-                ##print(f)
 
-
+if f==0:
+        print("prime")
+else:
+        print("composite")
